01aed0b64e41c70ceb638391638064b242375d73
dizaam <luthfimaajid@gmail.com>
2021-05-18 21:45:26 +0700

    fix ignore

--- exercism/cpp/collatz-conjecture/collatz_conjecture.cpp
+++
@@ -1,8 +0,0 @@
-#include "collatz_conjecture.h"
-
-namespace collatz_conjecture {
-    int collatz_conjecture(int num){
-        
-    }
-
-}  // namespace collatz_conjecture
--- exercism/cpp/reverse-string/reverse_string.cpp
+++
@@ -1,9 +0,0 @@
-#include "reverse_string.h"
-
-namespace reverse_string {
-
-    std::string reverse_string(const std::string &word){
-        return std::string(word.rbegin(), word.rend());
-    }
-
-}  // namespace reverse_string
